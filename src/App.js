1b62ac66fcdd510baf93a15e0a7c012eba680245
= <abhisingh30800@gmail.com>
2022-09-26 19:33:56 +0530

    remoed env variable

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ function App() {
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
 
-  // const apiKey = 'at_cbFrRvCQf8x7PL9FASi1jvf3LomDr';
-  const apiKey = process.env.REACT_APP_IP_API_KEY;
+  const apiKey = 'at_cbFrRvCQf8x7PL9FASi1jvf3LomDr';
 
   const fetchLocation = async (ip) => {
     const url = `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${apiKey}&ipAddress=${ip}`;
