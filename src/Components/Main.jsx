c5f8db72d690e300a1dd281b616ee4a9e0afce43
= <abhisingh30800@gmail.com>
2022-09-27 10:03:34 +0530

    button accessibility issue solved

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -9,13 +9,14 @@ const Main = ({ ip, location, timezone, isp, handleIpInput }) => {
         handleIpInput(ipValue);
         console.log(ipValue);
     }
+    const buttonValue = ">";
     return (
         <main>
             <h1 className="heading">IP Address Tracker</h1>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="ip"></label>
                 <input value={ipValue} onChange={(e) => setIpValue(e.target.value)} type="text" id="ip" name="ip" placeholder='search for any IP address or any domain' />
-                <button type="submit"><img src="./images/icon-arrow.svg" alt="arrow-icon" aria-hidden="true" /></button>
+                <button type="submit">{buttonValue}</button>
             </form>
             <InfoCard ip={ip} location={location} timezone={timezone} isp={isp} />
         </main>
